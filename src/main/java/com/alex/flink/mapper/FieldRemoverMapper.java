ac1261ebb9666ca0543b8536b4c07a8249a43b6e
Alex Sánchez <alejandro.sanchez.cabana@gmail.com>
2025-05-03 11:42:20 +0200

    Rename Element to Field

--- src/main/java/com/alex/flink/mapper/FieldRemoverMapper.java
+++ src/main/java/com/alex/flink/mapper/FieldRemoverMapper.java
@@ -9,23 +9,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FieldRemoverMapper implements MapFunction<List<Object>, List<Object>> {
-  private final String elementToRemove;
+  private final String fieldToRemove;
 
-  public FieldRemoverMapper(String elementToRemove) {
-    this.elementToRemove = elementToRemove;
+  public FieldRemoverMapper(String fieldToRemove) {
+    this.fieldToRemove = fieldToRemove;
   }
 
   @Override
   public List<Object> map(List<Object> objects) throws Exception {
     List<Object> resultList = new ArrayList<>();
-    for (Object jsonObject : objects) {
-      ObjectMapper objectMapper = new ObjectMapper();
-      JsonNode jsonNode = objectMapper.readTree(jsonObject.toString());
+    if (!fieldToRemove.isEmpty()) {
+      for (Object jsonObject : objects) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(jsonObject.toString());
 
-      if (jsonNode.has(elementToRemove)) {
-        ((ObjectNode) jsonNode).remove(elementToRemove);
+        if (jsonNode.has(fieldToRemove)) {
+          ((ObjectNode) jsonNode).remove(fieldToRemove);
+        }
+        resultList.add(objectMapper.writeValueAsString(jsonNode));
       }
-      resultList.add(objectMapper.writeValueAsString(jsonNode));
     }
     return resultList;
   }
