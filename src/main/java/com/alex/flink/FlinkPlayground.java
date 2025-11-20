c06d1361f449383bfe7cb845f79917bf9389f952
Alex Sánchez <alejandro.sanchez.cabana@gmail.com>
2025-11-20 14:04:52 +0100

    Fix main

--- src/main/java/com/alex/flink/FlinkPlayground.java
+++ src/main/java/com/alex/flink/FlinkPlayground.java
@@ -6,21 +6,17 @@ import com.alex.flink.sources.disk.DiskSource;
 import com.alex.flink.sources.s3.S3Source;
 import com.alex.flink.utils.MarkdownProperties;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.functions.JoinFunction;
 import org.apache.flink.api.connector.sink2.Sink;
 import org.apache.flink.api.connector.source.Source;
 import org.apache.flink.api.connector.source.SourceSplit;
-import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.core.execution.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.URL;
-import java.time.Duration;
 import java.util.List;
 
 public class FlinkPlayground {
@@ -41,10 +37,6 @@ public class FlinkPlayground {
     addSource(env, DiskSource.class);
 
     String fieldToRemove = properties.getProperty("field.remover.fieldToRemove");
-    DataStreamSource<List<Object>> secondaryStream = env.fromSource(new DiskSource("/Users/alex/data/authors/", "*.json"), WatermarkStrategy.forMonotonousTimestamps(), "Disk Source");
-
-    dataStream.join(secondaryStream).where(KeySelector<>).equalTo("id")
-        .window(TumblingProcessingTimeWindows.of(Duration.ZERO));
     SingleOutputStreamOperator<List<Object>> stream = dataStream.map(new FieldRemoverMapper(fieldToRemove));
 
     addSink(stream, DiskSink.class);
@@ -52,22 +44,26 @@ public class FlinkPlayground {
     env.execute("Flink Job");
   }
 
-  private static void addSource(StreamExecutionEnvironment env, Class<? extends Source<List<Object>, ? extends SourceSplit, Void>> source) {
+  private static void addSource(StreamExecutionEnvironment env,
+      Class<? extends Source<List<Object>, ? extends SourceSplit, Void>> source) {
     if (source.equals(S3Source.class)) {
       String bucketName = properties.getProperty("s3.bucket.name");
       String bucketRegion = properties.getProperty("s3.bucket.region", "eu-west-1");
       String filePath = properties.getProperty("s3.file.path");
       String s3AccessKey = properties.getProperty("aws.access.key");
       String s3SecretKey = properties.getProperty("aws.secret.key");
-      dataStream = env.fromSource(new S3Source(bucketName, bucketRegion, filePath, s3AccessKey, s3SecretKey), WatermarkStrategy.forMonotonousTimestamps(), "S3 Source");
+      dataStream = env.fromSource(new S3Source(bucketName, bucketRegion, filePath, s3AccessKey, s3SecretKey),
+          WatermarkStrategy.forMonotonousTimestamps(), "S3 Source");
     } else if (source.equals(DiskSource.class)) {
       String inputPath = properties.getProperty("local.input.dir");
       String inputPattern = properties.getProperty("local.input.pattern");
-      dataStream = env.fromSource(new DiskSource(inputPath, inputPattern), WatermarkStrategy.forMonotonousTimestamps(), "Disk Source");
+      dataStream = env.fromSource(new DiskSource(inputPath, inputPattern), WatermarkStrategy.forMonotonousTimestamps(),
+          "Disk Source");
     }
   }
 
-  private static void addSink(SingleOutputStreamOperator<List<Object>> stream, Class<? extends Sink<List<Object>>> sink) {
+  private static void addSink(SingleOutputStreamOperator<List<Object>> stream,
+      Class<? extends Sink<List<Object>>> sink) {
     if (sink.equals(DiskSink.class)) {
       String outputPath = properties.getProperty("local.output.dir");
       stream.sinkTo(new DiskSink(outputPath));
@@ -87,7 +83,8 @@ public class FlinkPlayground {
       properties.load(new FileInputStream(appConfigPath + "applicationd.md"));
     } catch (IOException e) {
       System.out.println("Failed to locate application.md");
-      System.out.println("Please make sure /src/main/resources/application.md exists, if it doesn't you can create by copying the default_application.md file");
+      System.out.println(
+          "Please make sure /src/main/resources/application.md exists, if it doesn't you can create by copying the default_application.md file");
       throw new RuntimeException();
     }
   }
